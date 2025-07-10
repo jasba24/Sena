d7ceefc0e6229438dab2f5106055cb72eca30847
jasba24 <johanbuitrago342@gmail.com>
2025-07-10 17:06:00 -0500

    add final componentes and pages

---
+++ src/pages/shopping.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import '../components/styles/shopping.css'
+
+
+function Shooping() {
+  const imgs = []
+  for (let i = 0; i < localStorage.length; i++) {
+    console.log(localStorage.length)
+    imgs.push(localStorage.getItem(i))
+  }
+  
+  return (
+    <div>
+      <h1>Tus productos son los siguientes:</h1>
+      <div className="section-container">
+        {imgs.map((img, i) => {
+          return (
+            <a href="">
+              <img
+                className="shopping-img product-image"
+                key={i}
+                src={img}
+                alt=""
+              />
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Shooping
