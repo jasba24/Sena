cef004f84d31e950c4902589cef1d9a72a7ec4eb
jasba24 <johanbuitrago342@gmail.com>
2025-07-10 15:17:31 -0500

    add home styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './components/styles/index.css'
+import { initializeApp } from 'firebase/app'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJD4Fn54Cj39RAkt9o6IQEnfln1M-RrfE',
+  authDomain: 'test-55f80.firebaseapp.com',
+  projectId: 'test-55f80',
+  storageBucket: 'test-55f80.appspot.com',
+  messagingSenderId: '747039356086',
+  appId: '1:747039356086:web:abefd0d9eeeca34018e217'
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App /> )
