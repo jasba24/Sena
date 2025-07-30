b742c5f580239a6aaa7d0bc29f952058c8586856
jasba24 <johanbuitrago342@gmail.com>
2025-07-30 09:43:58 -0500

    add main component refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './components/styles/index.css'
-import { initializeApp } from 'firebase/app'
+import React, { StrictMode } from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./components/styles/index.css"
+import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDJD4Fn54Cj39RAkt9o6IQEnfln1M-RrfE',
-  authDomain: 'test-55f80.firebaseapp.com',
-  projectId: 'test-55f80',
-  storageBucket: 'test-55f80.appspot.com',
-  messagingSenderId: '747039356086',
-  appId: '1:747039356086:web:abefd0d9eeeca34018e217'
+  apiKey: "AIzaSyDJD4Fn54Cj39RAkt9o6IQEnfln1M-RrfE",
+  authDomain: "test-55f80.firebaseapp.com",
+  projectId: "test-55f80",
+  storageBucket: "test-55f80.appspot.com",
+  messagingSenderId: "747039356086",
+  appId: "1:747039356086:web:abefd0d9eeeca34018e217",
 }
 
 export const firebaseApp = initializeApp(firebaseConfig)
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App /> )
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
