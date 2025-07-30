96dccadcea234f1ea0545e3f8d339e30f7babcfd
jasba24 <johanbuitrago342@gmail.com>
2025-07-30 11:32:25 -0500

    remove unnecesarry files

--- src/pages/shoes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../components/styles/product.css'
-import ShoesList from '../components/shoesList'
-
-function Shoes() {
-  return <ShoesList></ShoesList>
-}
-
-export default Shoes
