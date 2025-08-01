df22a6f18fd2777668fccc6c76a5e6c3806b90eb
jasba24 <johanbuitrago342@gmail.com>
2025-08-01 13:36:52 -0500

    add middlewares

--- api/index.js
+++ api/index.js
@@ -2,10 +2,17 @@ require("dotenv").config()
 require("./mongo")
 const express = require("express")
 const cors = require("cors")
+const logger = require("./middleware/logger")
+const NotFound = require("./middleware/NotFound")
+const HandlerError = require("./middleware/HandlerError")
+
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(logger)
+app.use(NotFound)
+app.use(HandlerError)
 
 const PORT = process.env.PORT || 3001
 const server = app.listen(PORT, () =>
