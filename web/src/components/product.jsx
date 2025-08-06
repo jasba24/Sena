b408c003c6958b584cd142e5b1a8a9eeff89dbee
jasba24 <johanbuitrago342@gmail.com>
2025-08-06 10:18:35 -0500

    change link styles

--- web/src/components/product.jsx
+++ web/src/components/product.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react"
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 
 function Product(props) {
   // eslint-disable-next-line react/prop-types
   const { image, name, id } = props
   return (
-    <Link to={`/${id}`}>
+    <section>
       <img src={image} alt="referencia del producto" />
       <h2>{name}</h2>
-      <button className="buy-button">ver productos</button>
-    </Link>
+      <Link to={`/${id}`}>
+        <button className="buy-button">ver productos</button>
+      </Link>
+    </section>
   )
 }
 
