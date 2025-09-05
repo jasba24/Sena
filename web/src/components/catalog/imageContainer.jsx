64d2e37017f40eb1171d1ef94bbbe8fd35d13cf3
jasba24 <johanbuitrago342@gmail.com>
2025-09-05 16:49:05 -0500

    add button to add to cart and the price

--- web/src/components/catalog/imageContainer.jsx
+++ web/src/components/catalog/imageContainer.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react"
 import { getImagesByCategory } from "../../services/images"
 import { useParams } from "react-router-dom"
+import Loading from "../layout/loading"
+import { useCart } from "../../context/CartContext"
 
 function ImageContainer() {
   const [images, setImages] = useState([])
+  const [loading, setLoading] = useState(true)
+  const { addToCart } = useCart()
   const route = useParams()
+
   useEffect(() => {
     const fetchImages = async () => {
       const images = await getImagesByCategory(route.category)
       setImages(images)
+      setLoading(false)
+      console.log(images.length)
     }
     fetchImages()
   }, [route.category])
@@ -16,15 +23,29 @@ function ImageContainer() {
   return (
     <div className="product-container">
       <h1>{route.category}</h1>
-      <div className="center-container">
-        {images.map((img) => (
-          <img
-            key={img._id}
-            className="product-image"
-            src={`data:${img.contentType};base64,${img.data}`}
-            alt={img.name}
-          />
-        ))}
+      <div className="center-container section-container">
+        {!loading && images.length === 0 && (
+          <div className="error message">
+            No hay productos disponibles en este momento.
+          </div>
+        )}
+        {loading ? (
+          <Loading message="Cargando productos:" />
+        ) : (
+          images.map((img) => (
+            <section key={img._id}>
+              <img
+                className="product-image"
+                src={`data:${img.contentType};base64,${img.data}`}
+                alt={img.name}
+              />
+              <h2>${img.price}</h2>
+              <button className="buy-button" onClick={() => addToCart(img)}>
+                Agregar al carrito
+              </button>
+            </section>
+          ))
+        )}
       </div>
     </div>
   )
